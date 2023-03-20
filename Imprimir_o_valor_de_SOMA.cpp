8c63dba782fa026e2f17cfe35385f6bd22fab740
GustaHSR <126812739+GustaHSR@users.noreply.github.com>
2023-03-20 10:35:07 -0300

    Create Imprimir_o_valor_de_SOMA.cpp

---
+++ Imprimir_o_valor_de_SOMA.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define f first
+#define s second
+#define ll long long
+#define ld long double
+
+using namespace std;
+
+int main() {
+    int i = 13, SOMA = 0, K = 0; //i é o INDICE
+
+ 	while( K < i) //Faz a soma enquanto a variável "k" for menor que a variável "i"
+	{ 
+
+		K = K + 1; 
+
+		SOMA = SOMA + K; 
+
+	} 
+
+ 	cout << SOMA; //Imprime a soma
+
+  //"SOMA" é igual a 91
+
+}
